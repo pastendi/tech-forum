8c2505becc4c474f31d5550d5e6a7c22cf1b1c68
Tendi <pastendisherpa26@gmail.com>
2023-02-28 20:24:06 +0545

    dynamic content in templates with dummy data

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='blog'),
+    path('', views.index, name='blog-index'),
     path('posts', views.all_posts, name='blog-posts'),
     path('posts/<slug:slug>', views.single_post, name='blog-single-post'),
 ]
